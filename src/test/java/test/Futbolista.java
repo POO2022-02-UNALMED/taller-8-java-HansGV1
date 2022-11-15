1b45e93df354c2c458eeba16d214c62d0484179e
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-11-15 11:55:12 -0500

    Up

--- src/test/java/test/Futbolista.java
+++ src/test/java/test/Futbolista.java
@@ -1,41 +1,39 @@
-package test;
-
-public class Futbolista implements Comparable<Futbolista>{
+package futbol;
+public abstract class Futbolista implements Comparable<Object>{
 	private String nombre;
 	private int edad;
 	private final String posicion;
 	
 	public Futbolista(String nombre, int edad, String posicion) {
-		this.nombre = nombre;
-		this.edad = edad;
-		this.posicion = posicion;
-	}
+		this.nombre=nombre;
+		this.edad=edad;
+		this.posicion=posicion;}
 	
-	public Futbolista(){
-		Futbolista futbolista1 = new Futbolista("Maradona", 30,"delantero");
-	}
+	public Futbolista() {
+		this.nombre="Maradona";
+		this.edad=30;
+		this.posicion="delantero";}
 	
-	public boolean equals(Futbolista f) {}
+	public boolean equals(Futbolista f) {
+		return this==f;}
+		
+	public abstract boolean jugarConLasManos();
+	
+	public String toString() {
+		return "El futbolista "+this.nombre+" tiene "+this.edad+ ", y juega de "+this.posicion;}
 	
 	public String getNombre() {
-		return this.nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+		return nombre;}
+	
 	public int getEdad() {
-		return this.edad;
-	}
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
+		return edad;}
+	
 	public String getPosicion() {
-		return this.posicion;
-	}
+		return posicion;}
 	
-	@Override
-	public String toString(){
-		return "El futbolista" + this.getNombre() + "tiene" + this.getEdad() + ", y juega de " + this.getPosicion();
-	}
+	public void setNombre(String nuevo) {
+		nombre=nuevo;}
 	
+	public void setEdad(int nuevo) {
+		edad=nuevo;}
 }
